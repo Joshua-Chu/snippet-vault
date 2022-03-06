902dd2a3f3e331aa46d7d7ea3004d4afff38ddc2
Joshua-Chu <jpchu@up.edu.ph>
2022-03-06 19:46:34 +0800

    test husky
    test husky
    
    add lint stage
    
    test husky2

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-	Width: 4,
-	ntWidth: 80,
-	OfLine: "auto",
-	owParens: "avoid",
-	ilingComma: "es5",
-	i: true,
-	Tabs: false,
-	gleQuote: false,
-	cketSpacing: true,
+  Width: 4,
+  ntWidth: 80,
+  OfLine: "auto",
+  owParens: "avoid",
+  ilingComma: "es5",
+  i: true,
+  Tabs: false,
+  gleQuote: false,
+  cketSpacing: true,
 };
-
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ const Home = () => {
   return (
     <>
       <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   );
 };
